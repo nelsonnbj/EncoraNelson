cd46bc2f1dc2a06391a021f25bc4513f60f7ec0a
nelson <ing.nelsonbatista@gmail.com>
2023-08-18 11:39:47 -0400

    Agregando Proyecto

---
+++ EncoraTestString/EncoraTestString/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncoraTestString
+{
+   public class Program
+    {
+        public static void Main(string[] args)
+        {
+            StringTest test = new StringTest();
+            Console.WriteLine("Write the list of string in each case"); 
+            Console.WriteLine("");
+            Console.WriteLine("Write Test Case 1");            
+            Console.WriteLine(test.SortStringByCharFrequencyTest1(Console.ReadLine().Split()));
+            Console.WriteLine("");
+            Console.WriteLine("Write Test Case 2");
+            Console.WriteLine(test.SortStringByCharFrequencyTest2(Console.ReadLine().Split()));
+            Console.WriteLine("");
+            Console.WriteLine("Write Test Case 3");
+            Console.WriteLine(test.SortStringByCharFrequencyTest3(Console.ReadLine().Split()));
+            Console.WriteLine("");
+            Console.WriteLine("Write Test Case 4");
+            Console.WriteLine(test.SortStringByCharFrequencyTest4(Console.ReadLine().Split()));
+            Console.WriteLine("");
+            Console.WriteLine("Write Test Case 5");
+            Console.WriteLine(test.SortStringByCharFrequencyTest5(Console.ReadLine().Split()));
+            Console.WriteLine("");
+            Console.WriteLine("Finish all the test");
+        }
+    }
+}
---
+++ EncoraTestString/EncoraTestString/Unit Test/StringTest.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncoraTestString
+{
+   public class StringTest
+    {
+
+        [TestMethod()]
+        public string SortStringByCharFrequencyTest1(string[] input)
+        {
+            string[] expectedOutput = { "gggdlquvwz", "tttgjmrwxy", "nnghilptux", "cgijkoptvz","ddppwwaest",
+                                        "ccppbiklns", "ffhhblmtvy", "rrrttacnqx", "ccnnadmort", "ffkkdegnst" };
+    
+            SortingOperations sortString = new SortingOperations();        
+            for (int i = 0; i < input.Length; i++)
+            {
+                var result = sortString.SortStringByCharFrequency(input[i]);
+               
+                //Assert.Equals(expectedOutput[i], result);
+                if (expectedOutput[i].ToString() != result)
+                  {
+                      return String.Format("Expected Out: {0}  Status : {1}", string.Join(",", expectedOutput),"Failed");                
+                  }
+            }
+            return String.Format("Expected Out: {0}  Status : {1}", string.Join(",", expectedOutput), "OK");
+        }
+
+        [TestMethod()]
+        public string SortStringByCharFrequencyTest2(string[] input)
+        {
+           string[] expectedOutput = { "eeuuwwabnz", "fklnovwxyz", "eedfklnrxy", "ddafgioqsw", "ccbfikuvyz" };
+
+            SortingOperations sortString = new SortingOperations();
+            for (int i = 0; i < input.Length; i++)
+            {
+                var result = sortString.SortStringByCharFrequency(input[i]);
+                // Assert
+                if (expectedOutput[i].ToString() != result)
+                {
+                    return String.Format("Expected Out: {0}  Status : {1}", string.Join(",", expectedOutput), "Failed");
+                }
+            }
+            return String.Format("Expected Out: {0}  Status : {1}", string.Join(",", expectedOutput), "OK");
+        }
+
+        [TestMethod()]
+        public string SortStringByCharFrequencyTest3(string[] input)
+        {
+           string[] expectedOutput = { "ackmq", "rrbkt", "ainvx", "jmnpw", "iopru", "abkst", "ejqrw", "llegw", "ooaiy", "hknqr" };
+
+            SortingOperations sortString = new SortingOperations();
+            for (int i = 0; i < input.Length; i++)
+            {
+                var result = sortString.SortStringByCharFrequency(input[i]);
+                // Assert
+                if (expectedOutput[i].ToString() != result)
+                {
+                    return String.Format("Expected Out: {0}  Status : {1}", string.Join(",", expectedOutput), "Failed");
+                }
+            }
+            return String.Format("Expected Out: {0}  Status : {1}", string.Join(",", expectedOutput), "OK");
+        }
+
+        [TestMethod()]
+        public string SortStringByCharFrequencyTest4(string[] input)
+        {
+           string[] expectedOutput = { "ijmpz", "nnfjq", "hosxy" };
+
+            SortingOperations sortString = new SortingOperations();
+            for (int i = 0; i < input.Length; i++)
+            {
+                var result = sortString.SortStringByCharFrequency(input[i]);
+                // Assert
+                if (expectedOutput[i].ToString() != result)
+                {
+                    return String.Format("Expected Out: {0}  Status : {1}", string.Join(",", expectedOutput), "Failed");
+                }
+            }
+            return String.Format("Expected Out: {0}  Status : {1}", string.Join(",", expectedOutput), "OK");
+        }
+
+        [TestMethod()]
+        public string SortStringByCharFrequencyTest5(string[] input)
+        {
+             string[] expectedOutput = { "cqsxy", "abeox", "afkos", "bdilt", "grswy" };
+
+            SortingOperations sortString = new SortingOperations();
+            for (int i = 0; i < input.Length; i++)
+            {
+                var result = sortString.SortStringByCharFrequency(input[i]);
+                // Assert
+                if (expectedOutput[i].ToString() != result)
+                {
+                    return String.Format("Expected Out: {0}  Status : {1}", string.Join(",", expectedOutput), "Failed");
+                }
+            }
+            return String.Format("Expected Out: {0}  Status : {1}", string.Join(",", expectedOutput), "OK");
+        }
+
+    }
+}
